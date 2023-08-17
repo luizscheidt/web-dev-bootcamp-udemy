bdfea1880a6513f0aba039c98ae97933f07feb08
Lipe Scheidt <luizfscheidt2@gmail.com>
2023-08-16 22:47:43 -0300

    criação primeiro modelo

---
+++ app.js
@@ -0,0 +1,17 @@
+const form = document.querySelector("form")
+const input = document.querySelector("#album")
+const lista = document.querySelector("#lista")
+const num = document.querySelector("#nota")
+
+form.addEventListener("submit", function(evt) {
+    evt.preventDefault();
+    const album = input.value;
+    const nota = num.value;
+    const newLI = document.createElement('li');
+    newLI.innerHTML = `${album} ${nota}`;
+    lista.append(newLI)
+});
+
+lista.addEventListener ('click', function(evt){
+    evt.target.remove();
+})
