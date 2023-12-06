fb4af8545cf34e17c0f5b5190ee6303f8fc4cdbc
Lipe Scheidt <luizfscheidt2@gmail.com>
2023-12-06 17:27:30 -0300

    Adding the wrapAsync function

--- Farm Stand/index.js
+++ Farm Stand/index.js
@@ -3,6 +3,7 @@ const app = express();
 const path = require("path");
 const mongoose = require("mongoose");
 const methodOverride = require("method-override");
+
 const AppError = require("./AppError");
 
 const Product = require("./models/product");
@@ -20,86 +21,87 @@ mongoose
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
 
-app.use(express.urlencoded({ extended: true }));
+app.use(express.urlencoded({extended: true}));
 app.use(methodOverride("_method"));
 
 const categories = ["fruit", "vegetables", "dairy"];
 
-app.get("/products", async (req, res, next) => {
-  try {
-    const { category } = req.query;
+function wrapAsync(fn) {
+  return function (req, res, next) {
+    fn(req, res, next).catch((e) => next(e));
+  };
+}
+
+app.get(
+  "/products",
+  wrapAsync(async (req, res, next) => {
+    const {category} = req.query;
     if (category) {
-      const products = await Product.find({ category });
-      res.render("products/index", { products, category });
+      const products = await Product.find({category});
+      res.render("products/index", {products, category});
     } else {
       const products = await Product.find({});
-      res.render("products/index", { products, category: "All" });
+      res.render("products/index", {products, category: "All"});
     }
-  } catch (e) {
-    next(e);
-  }
-});
+  })
+);
 
 app.get("/products/new", (req, res) => {
-  res.render("products/new", { categories });
+  res.render("products/new", {categories});
 });
 
-app.post("/products", async (req, res, next) => {
-  try {
+app.post(
+  "/products",
+  wrapAsync(async (req, res, next) => {
     const newProduct = new Product(req.body);
     await newProduct.save();
     res.redirect(`/products/${newProduct._id}`);
-  } catch (e) {
-    next(e);
-  }
-});
+  })
+);
 
-app.get("/products/:id", async (req, res, next) => {
-  try {
-    const { id } = req.params;
+app.get(
+  "/products/:id",
+  wrapAsync(async (req, res, next) => {
+    const {id} = req.params;
     const product = await Product.findById(id);
     if (!product) {
       throw new AppError("Product not found", 404);
     }
-    res.render("products/details", { product });
-  } catch (e) {
-    next(e);
-  }
-});
+    res.render("products/details", {product});
+  })
+);
 
-app.get("/products/:id/edit", async (req, res, next) => {
-  try {
-    const { id } = req.params;
+app.get(
+  "/products/:id/edit",
+  wrapAsync(async (req, res, next) => {
+    const {id} = req.params;
     const product = await Product.findById(id);
     if (!product) {
       throw new AppError("Product not found", 404);
     }
-    res.render("products/edit", { product, categories });
-  } catch (e) {
-    next(e);
-  }
-});
+    res.render("products/edit", {product, categories});
+  })
+);
 
-app.put("/products/:id", async (req, res, next) => {
-  try {
-    const { id } = req.params;
+app.put(
+  "/products/:id",
+  wrapAsync(async (req, res, next) => {
+    const {id} = req.params;
     const product = await Product.findByIdAndUpdate(id, req.body, {
       runValidators: true,
     });
     res.redirect(`/products/${product._id}`);
-  } catch (e) {
-    next(e);
-  }
-});
+  })
+);
 
 app.delete("/products/:id", async (req, res) => {
-  const { id } = req.params;
+  const {id} = req.params;
   const deletedProduct = await Product.findByIdAndDelete(id);
   res.redirect("/products");
 });
 
 app.use((err, req, res, next) => {
-  const { status = 500, message = "Something went wrong" } = err;
+  const {status = 500, message = "Something went wrong"} = err;
   res.status(status).send(message);
 });
 
