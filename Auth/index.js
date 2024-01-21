68a1f4419169a9cf153273af5a82c96fe60806b1
Lipe Scheidt <luizfscheidt2@gmail.com>
2024-01-21 00:08:18 -0300

    Adding the hash algorithm

--- Auth/index.js
+++ Auth/index.js
@@ -2,9 +2,22 @@ const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
 const User = require("./models/user");
+const bcrypt = require("bcrypt");
+
+mongoose
+  .connect("mongodb://127.0.0.1:27017/authDemo")
+  .then(() => {
+    console.log("Mongo connection open");
+  })
+  .catch((err) => {
+    console.log("Something went wrong when connecting to mongo");
+    console.log(err);
+  });
 
 app.set("view engine", "ejs");
 app.set("views", "views");
+
+app.use(express.urlencoded({extended: true}));
 
 app.get("/secret", (req, res) => {
   res.send("Secreto");
@@ -12,6 +25,17 @@ app.get("/secret", (req, res) => {
 
 app.get("/register", (req, res) => {
   res.render("Register");
+});
+
+app.post("/register", async (req, res) => {
+  const {password, username} = req.body;
+  const hash = await bcrypt.hash(password, 12);
+  const user = new User({
+    username,
+    password: hash,
+  });
+  await user.save();
+  res.redirect("/");
 });
 
 app.listen(3000, () => {
