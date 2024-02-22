e4835703e4b72237238bfbbadc6f3d23c4096c5d
Lipe Scheidt <luizfscheidt2@gmail.com>
2024-02-22 15:02:08 -0300

    Working with common arrays updating patterns

--- REACT/stateDemo/src/EmojiClicker.jsx
+++ REACT/stateDemo/src/EmojiClicker.jsx
@@ -3,20 +3,33 @@ import {v4 as uuid} from "uuid"
 
 export default function EmojiClicker(){
     const [emojis, setEmojis] = useState([{id: uuid(), emoji: "🤩"}]);
+    
     const addEmoji = () => {
         setEmojis(oldEmojis =>[...oldEmojis, {id: uuid(), emoji: "🤠"}, {id: uuid(), emoji: "😇"}]) 
     };
+
     const deleteEmoji = (id) => {
         setEmojis(previousEmojis=> {
             return previousEmojis.filter(e => e.id !== id);
         })
     }
+
+    const allHearts = ()=> {   
+        setEmojis((previousEmojis)=> {
+            return previousEmojis.map((e)=> {
+                return {...e, emoji: '❤️'} 
+            })
+        })
+    }
+    
+
     return (
         <div>
             {emojis.map(e => (
                 <span onClick={()=> deleteEmoji(e.id)} key={e.id}>{e.emoji}</span>
             ))}
             <button onClick={addEmoji} >Add Emoji</button>
+            <button onClick={allHearts} >Make them all hearts</button>
         </div>
     )
 }
