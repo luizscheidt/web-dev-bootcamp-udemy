b683d0602e44345512a0d40fa0056019784278e8
Lipe Scheidt <luizfscheidt2@gmail.com>
2024-02-23 13:47:08 -0300

    Passing functions as props to make other challenges in the game

--- REACT/luckyNum/src/App.jsx
+++ REACT/luckyNum/src/App.jsx
@@ -1,11 +1,22 @@
 import Dice from './Dice'
 import LuckyN from './LuckyN'
+import { getRolls, sum } from "./utils";
+
+
+const lessThen4 = (dice)=> {
+  return sum(dice) < 4;
+}
+
+const sameDice = (dice)=>{
+  return dice.every((v)=> v === dice[0])
+}
 
 function App() {
 
   return (
     <>
-      <LuckyN numDice={4} target={15}/>
+      <LuckyN numDice={2} winFunction={lessThen4}/>
+      <LuckyN numDice={2} winFunction={sameDice}/>
     </>
   )
 }
--- REACT/luckyNum/src/LuckyN.jsx
+++ REACT/luckyNum/src/LuckyN.jsx
@@ -2,15 +2,15 @@ import Dice from "./Dice";
 import { useState } from "react";
 import { getRolls, sum } from "./utils";
 
-export default function LuckyN({numDice=2, target=7}){
+export default function LuckyN({numDice=2, winFunction}){
     const [dice, setDice] = useState(getRolls(numDice));
-    const isWinner = sum(dice) === target;
+    const isWinner = winFunction(dice)
     const roll = ()=>{
         setDice(getRolls(numDice))
     }
     return (
         <main className="LuckyN">
-            <h1>Lucky{target}</h1>
+            <h1>Lucky</h1>
             <Dice dice={dice}/>
             {isWinner && <h2>You Won!!</h2>}
             <button onClick={roll}>Roll the dice again</button>
