ce0e8a1991616513598343d9cf2c97ea518b6d1b
Lipe Scheidt <luizfscheidt2@gmail.com>
2024-03-03 15:18:51 -0300

    Saving the changes in localStorage

--- REACT/toDoList/src/TodoList.jsx
+++ REACT/toDoList/src/TodoList.jsx
@@ -1,17 +1,23 @@
 import List from '@mui/material/List';
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import TodoItem from './TodoItem';
 import TodoForm from './TodoForm';
 
-const initialTodos = [
-    {id: 1, text: "Walk the dog", completed: false},
-    {id: 2, text: "Walk the cat", completed: false},
-    {id: 3, text: "Walk the fish", completed: true},
-    {id: 4, text: "Walk the bull", completed: true},
-    ]
+const getInitialData = () => { 
+    const data = JSON.parse(localStorage.getItem('todos'))
+    if(!data){return []}
+    return data;
+}
 
 export default function TodoList() {
-    const [todos, setTodos] = useState(initialTodos);
+    const [todos, setTodos] = useState(getInitialData);
+
+    useEffect(()=> {
+        localStorage.setItem(
+            'todos',
+            JSON.stringify(todos)
+        )
+    }, [todos]) 
 
     const removeTodo = (id) => {
         setTodos((prevTodos) => {
@@ -32,7 +38,7 @@ export default function TodoList() {
 
     const addTodo = (text) => {
         setTodos((prevTodos) => {
-            return [...prevTodos, {text: text, id: 6, completed: false}]
+            return [...prevTodos, {text: text, id: 6 , completed: false}]
         })
     }
 
