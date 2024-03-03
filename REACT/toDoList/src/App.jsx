6633197fc633628ed52ba5d7ca9e325d69b86f41
Lipe Scheidt <luizfscheidt2@gmail.com>
2024-03-03 11:07:36 -0300

    Creating the react to-do list

---
+++ REACT/toDoList/src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import CssBaseline from '@mui/material/CssBaseline'
+
+function App() {
+
+  return (
+    <>
+     <CssBaseline/>
+     <h1>TO DOS</h1>
+    </>
+  )
+}
+
+export default App
